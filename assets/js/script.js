825460ac8f34cb023c6b6710dda95ada2d911ca7
Akash R Chandran <chandranrakash@gmail.com>
2023-03-27 07:45:39 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,9 @@ const particles = Particles.init({
     selector: ".background",
     color: ["#03dac6", "#ff0266", "#000000"],
     connectParticles: true,
-    responsive: [
-        {
-            breakpoint: 768,
-            options: {
-                color: ["#faebd7", "#03dac6", "#ff0266"],
-                maxParticles: 43,
-                connectParticles: false
-            }
-        }
-    ]
+
+
+
 });
 
 class NavigationPage {
