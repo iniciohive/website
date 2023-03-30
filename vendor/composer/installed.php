809953181fabfebebd407a7f8dfb23a54216c9dc
Akash R Chandran <chandranrakash@gmail.com>
2023-03-30 23:09:24 +0530

    updated

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f2f31757c1540ccdc6c191498c989b7fd0a68c16',
+        'name' => 'uddhav/itcompany',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.0',
+            'version' => '6.8.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'df16b615e371d81fb79e506277faea67a1be18f1',
+            'dev_requirement' => false,
+        ),
+        'uddhav/itcompany' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f2f31757c1540ccdc6c191498c989b7fd0a68c16',
+            'dev_requirement' => false,
+        ),
+    ),
+);
