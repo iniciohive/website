0bd66d7c023b54fcc510fd61dd5794d74fcf4e33
Anandhukg <anandhukrishnang142@gmail.com>
2023-04-01 10:34:01 +0530

    change

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
         if($(window).scrollTop()>35)
         {
-            $('.header').css({'background':'#002e5f','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+            $('.header').css({'background':'#001b30','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
         }
         else
         {
